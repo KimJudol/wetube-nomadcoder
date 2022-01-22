8e83ce90f462e76517dc247fe4d239ab48adfd42
JK-cs <wskje0106@ewhain.net>
2022-01-23 04:21:06 +0900

    1/23_2

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
-import {watch, edit} from "../controllers/videoController";
+import {edit, upload, deleteVideo} from "../controllers/videoController";
+import {see} from "../controllers/userController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
+
 
 export default videoRouter;
