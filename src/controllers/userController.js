507bf16091e8e8e56baa34cde923b32232407547
JK-cs <wskje0106@ewhain.net>
2022-01-23 03:49:14 +0900

    1/23

---
+++ src/controllers/userController.js
@@ -0,0 +1,5 @@
+import req from "express/lib/request";
+
+export const join = (req, res) => res.send("Join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
