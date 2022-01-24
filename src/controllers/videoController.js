1cf6e8d91d1e255d8487dbab39f2fec1d8b8293f
JK-cs <wskje0106@ewhain.net>
2022-01-24 16:57:54 +0900

    5.4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
 export const see = (req, res) => {
     return  res.render("watch");
 }
-export const edit = (req, res) => res.render ("Edit");
+export const edit = (req, res) => res.render ("Edit", {pageTitle: "Edit"});
 
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
