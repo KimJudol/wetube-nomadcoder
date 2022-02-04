000f1e736a22f3c508adcc75d1849f77d10b4773
JK-cs <wskje0106@ewhain.net>
2022-02-05 03:09:22 +0900

    postEdit modyfying

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,7 +27,19 @@ export const getEdit = async (req, res) => {
 
 export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+    if (!video) {
+        return res.render("404", { pageTitle: "Videos not found." });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: hashtags
+            .split(",")
+            .map((word) => word.startsWith('#') ? word : `#${word}`),
+    });
+    await video.save();
     return res.redirect(`/videos/${id}`);
 };
 
