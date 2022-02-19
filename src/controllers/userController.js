50893fb97d630dfeb64b2bfe63d6b413909ee8ee
JK-cs <wskje0106@ewhain.net>
2022-02-19 19:51:22 +0900

    form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,17 +3,31 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
-export const postJoin = async(req, res) => { 
-    const { name, username, email, password, location } = req.body;
+export const postJoin = async (req, res) => {
+    const { name, username, email, password, password2,  location } = req.body;
+    const pageTitle = "Join";
+    if(password !== password2){
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "Password confimation does not match.",
+        });
+    }
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    if (exists) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "This username/email is already taken.",
+        });
+    }
     await User.create({
-        name, 
-        username, 
-        email, 
-        password, 
+        name,
+        username,
+        email,
+        password,
         location,
     });
     return res.redirect("/login");
- };
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
