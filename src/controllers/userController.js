8b2ca44bef6724312d585eb0b5703586da8bc8e7
JK-cs <wskje0106@ewhain.net>
2022-02-19 19:15:51 +0900

    Create Account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,20 @@
 import req from "express/lib/request";
+import User from "../models/User";
 
-export const join = (req, res) => res.send("Join");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = async(req, res) => { 
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name, 
+        username, 
+        email, 
+        password, 
+        location,
+    });
+    return res.redirect("/login");
+ };
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
