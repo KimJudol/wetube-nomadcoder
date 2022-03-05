ea0016b9b50e7745a317d8bd2852840dab06b3db
JK-cs <wskje0106@ewhain.net>
2022-03-05 21:27:54 +0900

    secret

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-import session from "express-session";
 import { localsMiddleware } from "./middlewares";
 
 const app=express();
@@ -16,9 +17,10 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(
     session({
-        secret: "Hello!",
-        resave: true,
-        saveUninitialized: true,
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
     })
 );
 
